75a54e52aa2ed7846ba52a82d2c2ed92f500931b
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2017-03-25 15:52:17 +0530

    working with arrays in scala.

--- HelloScala/src/WhileLoopInScala.scala
+++ HelloScala/src/WhileLoopInScala.scala
@@ -1,15 +1,16 @@
 
 
-class WhileLoopInScala {
+object WhileLoopInScala {
 
-	def main(args: Array[String]): Unit = {  //main method just like java, c, c++ 
-			println("method signature in scala program");
-			if(args.length>0){
+  def main(args: Array[String]): Unit = {
+    println("Implementing while loop in scala")
+    	if(args.length>0){
 				var i = 0;   //will be converted into Int as scala provide type inference feature
 				while(i<args.length){
-					print(args(i));
+					println(args(i));
+					i+=1;
 				}
 			}
-	} 
+  }
 
 }
---
+++ HelloScala/src/WorkingWithArray.scala
@@ -0,0 +1,29 @@
+
+
+object WorkingWithArray {
+  
+ 
+  def main(args: Array[String]): Unit = {
+    println("Welcome to array implementation");
+    var array = new Array[String](4);
+    
+    array(0)="Hello";
+    array(1)="My";
+    array(2)="Name";
+    array(3)=" Is Rajeev Shukla";
+    
+    
+    array.foreach(s=>println(s));
+   //  array.foreach(println);  another way to implement above statement.
+    
+    println("Another version of foreach");
+    array.foreach((s:String)=>println(s));
+    
+    //another version of for loop in scala. 
+    for(s<-array){
+      println(s);
+    }
+  }
+  
+  
+}
