6982e3dc768d2ec0a7ab0c4fc1a89d504986d5ee
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2017-03-25 13:26:17 +0530

    method signature in scala

---
+++ HelloScala/src/MethodSignatureInScala.scala
@@ -0,0 +1,22 @@
+
+
+object MethodSignatureInScala {
+  
+   def main(args: Array[String]): Unit = {  //main method just like java, c, c++ 
+    
+  } 
+ 
+    def method() = print("welcome")   //  another single line method syntax //return type Unit = void(in java)
+    
+    def method2() =  println("welcome2") ; // (semicolon optional)  //  another single line method signature.. //no equal
+    
+    def method3(x:Int, y:Int): Int = {    // parameterized method having method argument and return type as Int
+      if(x>y) 
+        return x
+      else 
+        return y
+    }
+
+    
+    
+}
