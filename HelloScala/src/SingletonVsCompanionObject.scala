7626fb455bba2f2d1b0c4fbd4c0336bca5d99c20
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2017-04-04 15:02:06 +0530

    Using for as a filter.

---
+++ HelloScala/src/SingletonVsCompanionObject.scala
@@ -0,0 +1,12 @@
+
+
+
+object singltonObject {
+  private var my:Int = 20;
+}
+
+object SingletonVsCompanionObject {
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
---
+++ HelloScala/src/UsingForAsAFilter.scala
@@ -0,0 +1,14 @@
+
+
+object UsingForAsAFilter {
+  
+  def main(args :Array[String]) { 
+    
+    var evenList = for { i <- 0 to 30 
+      if i%2==0 } yield i  //acting as a filter and yielding only i
+      
+      for(i<-evenList)
+        println(i)
+    
+  }
+}
