08abfc1f0f946a3686fa71517310ce97c779a98e
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2017-04-09 09:54:53 +0530

    Simplifying numberator and denominator in Rational class.

--- AdvancedScala/src/com/demo/rational/Rational.scala
+++ AdvancedScala/src/com/demo/rational/Rational.scala
@@ -2,15 +2,25 @@ package com.demo.rational
 
 class Rational(n:Int, d:Int) {
   require(d!=0) //precondition which must be full filled by the caller if false then illegel argument exception would be thrown.
-  val numer : Int = n;
-  val demon : Int = d;
-  override def toString = n+"/"+d
+  private val g = gcd(n.abs,d.abs)
+  val numer : Int = n/g
+  val demon : Int = d/g
+  
+  override def toString = numer+"/"+demon
+ 
   def add(that:Rational):Rational =
     new Rational(numer*that.demon+that.numer*d,d*that.demon)
+  
   def lessThan(that:Rational) = 
     this.numer*that.demon<that.numer*this.demon
-  def max(that:Rational)  = 
+  
+    def max(that:Rational)  = 
     if (this.lessThan(that)) that else this
+  
+    def gcd(a:Int,b:Int): Int = 
+      if(b==0)a else gcd(b,a%b)   //Ecluid's Algorithm to find GCD 
+    
+  
 }
 
 
@@ -18,8 +28,8 @@ object Main {
   def main(arg:Array[String]){
     
     var oneThird = new Rational(1,3);
-    var twoFourth = new Rational(2,4);
-    
+    var twoFourth = new Rational(22,44);
+    println(twoFourth)
     println("Result:"+ (oneThird add twoFourth))
     println("lessthan :" + (oneThird max twoFourth))
   }
